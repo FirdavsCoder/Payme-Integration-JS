b87b1349d0dba05bc889bb4c225369b76a7060bc
FirdavsCoder <firdavsyorkulov@gmail.com>
2024-02-12 18:28:12 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Payme Integration with Node.js
+This is a simple example of how to integrate Payme with Node.js.
+
+
+## Installation
+```bash
+npm install
+```
+
+## Usage
+```bash
+npm start
+```
+
+
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const { PORT } = require('./config/environments');
+const routes = require('./routes');
+const connectDB = require('./config/database');
+const errorHandler = require('./middlewares/error-handler.middleware');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api', routes);
+
+app.use(errorHandler);
+
+connectDB();
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
